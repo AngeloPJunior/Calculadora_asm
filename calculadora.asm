ba48e67a8eab1c1ebaaa30bc7ec3371bc1bacb26
Tcheludo <angelojunior09@gmail.com>
2022-11-06 01:24:57 -0300

    quase pronto

--- calculadora.asm
+++ calculadora.asm
@@ -3,10 +3,22 @@ TITLE ANGELO GERALDO PEREIRA JUNIOR RA:21008767
 .model small
 .stack 100h
 .data
-conta db 10,'Digite qual operaca_o vai ser realizada( + - * / )',10,': $'
-msg1 db 10,'Digite o primeiro numero: $'
-msg2 db 10,"Digite o segundo numero: $"
-resu db 10,'Resultado: $'
+menu     DB 10,10,    ' ESCOLHA A CONTA DESEJADA         ',10      
+         DB           '----------------------------      ',10
+         DB           '|           MENU           |      ',10
+         DB           '|                          |      ',10
+         DB           '| SOMA: 1                  |      ',10
+         DB           '| SUBTRACAO: 2             |      ',10
+         DB           '| MULTIPLICACAO: 3         |      ',10
+         DB           '| DIVISAO: 4               |      ',10
+         DB           '| SAIR: 5                  |      ',10
+         DB           ' ---------------------------      ',10
+         DB           'SUA ESCOLHA:  $'
+erro DB 10,'RESPOSTA INVALIDA, DIGITE NOVAMENTE '
+     DB 10,'SUA ESCOLHA:   $                    '
+msg1 DB 10,'DIGITE O PRIMEIRO NUMERO:  $'
+msg2 DB 10,10,'DIGITE O SEGUNDO NUMERO:  $'
+resu DB 10,'RESULTADO:  $'
 .code
 
 impmsg MACRO FUN,ende
@@ -21,76 +33,159 @@ ler macro FUN
 ENDM
 
 main proc
-    mov AX,@data
-   mov DS,AX
     ; mov AX,@data
     ; mov DS,AX       ;inicializando data
     ; lea DX,conta     ;offset msg1
     ; mov AH,09h      ;comando imprimir buffer msg1
     ; int 21h
-    impmsg 09,conta
+menu_1:
+    xor ax,ax
+    xor dx,dx
+    xor bx,bx
+    mov AX,@data
+    mov DS,AX
+    impmsg 09,menu
     ler 01
     mov bl,al
     
-    
-    cmp bl,'+'
+escolha: 
+    cmp bl,'1'
     jz soma
-    cmp bl,'-'
+
+    cmp bl,'2'
     jz subt
-    cmp bl,'*'
-    jz mult
-    cmp bl,'/'
-    jz divi
+    
+    cmp bl,'3'
+    jz mult_1
+    
+    cmp bl,'4'
+    jz divi_1
+    
+    cmp bl,'5'
+    jz saida_1
+    
+    impmsg 09,erro
+    ler 01
+    mov bl,al
+    jmp escolha
+    
+    
 
 
 soma:
     impmsg 09,msg1
     ler 01 
-    mov ch,al
+    mov bl ,al
+    and bl,0fh
     impmsg 09,msg2
     ler 01
-    mov cl,al
-    xor ax,ax
-    add ch,cl
+    mov bh,al
+    and bh,0fh
+    add bl,bh
+    impmsg 09,resu
+    ; mov ah,02
+    ; mov dl,'+'
+    ; int 21h
+    call print
+    jmp menu_1
+    
+mult_1:
+    jmp mult
+divi_1:
+    jmp divi
+saida_1:
     jmp saida
-    
-    
+
+
 
 subt:
     impmsg 09,msg1
+    ler 01 
+    mov bl ,al
+    and bl,0fh
+    
     impmsg 09,msg2
-    jmp saida
+    ler 01
+    mov bh,al
+    and Bh,0fh
+    
+    cmp bl,bh
+    jl negativo
+    sub bl,bh
+    impmsg 09,resu
+    ; mov ah,02
+    ; mov dl,'+'
+    ; int 21h
+    jmp printt
+
+negativo:
+    sub bl,Bh
+    neg bl
+    impmsg 09,resu
+    mov ah,02
+    mov dl,'-'
+    int 21h
+
+printt:
+    call print
+    jmp menu_1
 
 divi:
     impmsg 09,msg1
     impmsg 09,msg2
-    jmp saida
+    jmp menu_1
 
 mult:
     impmsg 09,msg1
+    ler 01 
+    mov bl ,al
+    and bl,0fh
     impmsg 09,msg2
-    jmp saida
+    ler 01
+    mov bh,al
+    and bh,0fh
+    xor cx,cx
+    mov CX,4
+    mov ah,0
+
+volta: 
+    ror bh,01
+    jc carry1
+    shl bl,1
+    jmp volta
+carry1:
+    add ah,bl
+    shl bl,1
+    loop volta
+    mov bl,ah
+    ;and bl,0fh
+    impmsg 09,resu
+    call print
+    jmp menu_1
+
+
 
 saida:
     mov AH,4ch
     int 21h
 
-print:
-    ;impmsg 09,resu
-    ;...
-    mov cl,10
-    div cl
-    mov bx,ax
+print proc
+    xor ax,ax
+    mov al,bl
+    mov ch,10
+    div ch
+    mov bl,al
+    mov bh,ah
     mov dl,bl
-    or dl,30h
+    xor dl,30h
     mov ah,2
     int 21h
-    mov dl,bl
-    mov dl,30h
+    mov dl,bh
+    xor dl,30h
     mov ah,2
     int 21h
-    jmp saida
-;print endp
+     ret
+ print endp
 
 main endp
 end main
