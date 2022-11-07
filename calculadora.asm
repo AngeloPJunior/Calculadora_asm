ea066edb715f8b8d7baac8a7c048f6fb303baa1d
Tcheludo <angelojunior09@gmail.com>
2022-11-07 09:33:12 -0300

    divisao pronta

--- calculadora.asm
+++ calculadora.asm
@@ -13,12 +13,17 @@ menu     DB 10,10,    ' ESCOLHA A CONTA DESEJADA         ',10
          DB           '| DIVISAO: 4               |      ',10
          DB           '| SAIR: 5                  |      ',10
          DB           ' ---------------------------      ',10
-         DB           'SUA ESCOLHA:  $'
+         DB           'SUA ESCOLHA:  $',10
 erro DB 10,'RESPOSTA INVALIDA, DIGITE NOVAMENTE '
      DB 10,'SUA ESCOLHA:   $                    '
 msg1 DB 10,'DIGITE O PRIMEIRO NUMERO:  $'
 msg2 DB 10,10,'DIGITE O SEGUNDO NUMERO:  $'
 resu DB 10,'RESULTADO:  $'
+quoci DB 10,'QUOCIENTE:  $'
+resto DB 10,'RESTO:  $'
+error DB 10,'DIVISAO COM ZERO, DIGITE NOVAMENTE',10,'$'
+novamente DB 10,'DESEJA FAZER OUTRA OPERACAO? (Y/N)',10
+          DB    ':  $'
 .code
 
 impmsg MACRO FUN,ende
@@ -31,6 +36,26 @@ ler macro FUN
     mov ah,FUN
     int 21h
 ENDM
+
+NUM1NUM2 macro
+    impmsg 09,msg1
+    ler 01 
+    mov bl ,al
+    and bl,0fh
+    impmsg 09,msg2
+    ler 01
+    mov bh,al
+    and bh,0fh
+ENDM
+    
+NOVAMENT macro 
+    impmsg 09,novamente
+    ler 01
+    cmp al,'y'
+    JZ menu_1
+    jmp saida
+ENDM
+
 
 main proc
     ; mov AX,@data
@@ -73,19 +98,9 @@ escolha:
 
 
 soma:
-    impmsg 09,msg1
-    ler 01 
-    mov bl ,al
-    and bl,0fh
-    impmsg 09,msg2
-    ler 01
-    mov bh,al
-    and bh,0fh
+    NUM1NUM2
     add bl,bh
     impmsg 09,resu
-    ; mov ah,02
-    ; mov dl,'+'
-    ; int 21h
     call print
     jmp menu_1
     
@@ -99,16 +114,7 @@ saida_1:
 
 
 subt:
-    impmsg 09,msg1
-    ler 01 
-    mov bl ,al
-    and bl,0fh
-    
-    impmsg 09,msg2
-    ler 01
-    mov bh,al
-    and Bh,0fh
-    
+    NUM1NUM2    
     cmp bl,bh
     jl negativo
     sub bl,bh
@@ -131,32 +137,72 @@ printt:
     jmp menu_1
 
 divi:
-    impmsg 09,msg1
-    impmsg 09,msg2
-    jmp menu_1
+NUM1NUM2
+    MOV CX,9
+    XOR AX,AX
+    MOV AL,BL    ;BL -> AL
+    CMP BH,0
+    JZ erro1
+    XOR BL,BL
+    XOR DX,DX
+LUP:
+    SUB AX,BX
+    JNS SINAL
+    ADD AX,BX
+    MOV DH,0
+    JMP TERMINA
+SINAL:
+    MOV DH,01
+TERMINA:
+    SHL DL,1
+    OR DL,DH
+    SHR BX,1
+    LOOP LUP
+    MOV CH,DL
+    MOV CL,AL
+    ; mov ah,09
+    ; lea dx,MSG4
+    ; int 21H
+
+    impmsg 09,quoci
+    or ch,30h
+    mov ah,02
+    mov dl,ch
+    int 21h
+
+    impmsg 09,resto
+    OR Cl,30H
+    MOV AH,02
+    MOV DL,Cl
+    INT 21H
+    JMP menu_1
+
+erro1:
+impmsg 09,error
+jmp divi
+
+
+
 
 mult:
-    impmsg 09,msg1
-    ler 01 
-    mov bl ,al
-    and bl,0fh
-    impmsg 09,msg2
-    ler 01
-    mov bh,al
-    and bh,0fh
+    NUM1NUM2
     xor cx,cx
     mov CX,4
     mov ah,0
 
-volta: 
+volta_1: 
     ror bh,01
-    jc carry1
+    jc carry
     shl bl,1
-    jmp volta
-carry1:
+    dec cx
+    cmp cx,0
+    jnz volta_1
+    jmp fim_mult
+carry:
     add ah,bl
     shl bl,1
-    loop volta
+    loop volta_1
+fim_mult:
     mov bl,ah
     ;and bl,0fh
     impmsg 09,resu
